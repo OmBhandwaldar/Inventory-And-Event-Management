2d9d26c38f4e15a3b9c532545c424dfd6ca0214b
Yashraj-Pradip-Haridas <yashraj.haridas22@pccoepune.org>
2024-10-20 16:45:48 +0530

    Delete frontend directory

--- frontend/src/components/UserView/UserView.jsx
+++
@@ -1,9 +0,0 @@
-import CalendarWithEvents from "../CalendarWithEvents/CalendarWithEvents";
-
-export default function UserView() {
-  return (
-    <div>
-      <CalendarWithEvents />
-    </div>
-  );
-}
