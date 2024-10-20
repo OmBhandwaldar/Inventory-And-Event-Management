5a3471c33b70b4cd5fce9229096ccb5c250a1c78
Yashraj-Pradip-Haridas <yashraj.haridas22@pccoepune.org>
2024-10-20 15:27:06 +0530

    app.tsx changes

--- frontendtypescript/src/App.tsx
+++ frontendtypescript/src/App.tsx
@@ -6,20 +6,17 @@ import Dashboard from "./ReportDashboard/Dashboard";
 import UserView from "./components/UserView/UserView";
 import AdminView from "./components/AdminView/AdminView";
 import EventDetails from "./components/EventDetails/EventDetails";
-<<<<<<< HEAD
 
 // import EventDetails from "./components/EventDetails/EventDetails";
 // import UserView from "./components/UserView/UserView";
 // import AdminView from "./components/AdminView/AdminView";
 // // import { Inventory } from "../../backend/db";
 // import InventoryReport from "./components/Reports/InventoryReport";
-=======
+
 import InventoryReport from "./components/Reports/InventoryReport";
 
-
-
 function App() {
-  const isAdmin = true;
+  const isAdmin = false;
   return (
     <>
       <Router>
@@ -28,15 +25,15 @@ function App() {
           <Route path="/login" element={<UserLogin />}></Route>
           <Route path="/signup" element={<UserSignup />}></Route>
           <Route path="/dashboard" element={<Dashboard />}></Route>
-<<<<<<< HEAD
+
           <Route path="/" element={isAdmin ? <AdminView /> : <UserView />} />
           <Route path="/event-details" element={<EventDetails />}></Route>
           {/* <Route path="/temp" element = {<InventoryReport/>}></Route> */}
-=======
+
           <Route path="/user-view" element={<UserView />}></Route>
           <Route path="/admin-view" element={<AdminView />}></Route>
           <Route path="/event-details" element={<EventDetails />}></Route>
-          <Route path="/temp" element = {<InventoryReport/>}></Route>
+          <Route path="/temp" element={<InventoryReport />}></Route>
         </Routes>
       </Router>
     </>
