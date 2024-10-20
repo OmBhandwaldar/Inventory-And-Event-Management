c2354c31962a4dd981eeacdae94b7ad80e18f8ee
Yashraj-Pradip-Haridas <yashraj.haridas22@pccoepune.org>
2024-10-20 21:17:35 +0530

    unused imports removed

--- frontendtypescript/src/Inventory/components/Sidebar.tsx
+++ frontendtypescript/src/Inventory/components/Sidebar.tsx
@@ -4,7 +4,7 @@ import {
   FaUsers,
   FaChartLine,
   FaCog,
-  FaSignOutAlt,
+  // FaSignOutAlt,
 } from "react-icons/fa";
 import { HiMenuAlt3 } from "react-icons/hi";
 
@@ -56,7 +56,7 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
         {/* Sidebar Header */}
         <div className="flex justify-between items-center p-4 bg-gray-800">
           <span className={`text-xl font-bold ml-6 ${!isOpen && "hidden"}`}>
-            {isOpen && 'Close'}
+            {isOpen && "Close"}
           </span>
           <button onClick={toggleSidebar}>
             <HiMenuAlt3 className="text-3xl" />
